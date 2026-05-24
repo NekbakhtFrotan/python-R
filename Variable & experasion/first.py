a50d82fbc31bd8790a1b23e13bd609ce6f8ad45d
Nekbakht Frotan <nekbakht.frotan1380@gmail.com>
2026-05-23 21:49:47 -0700

    first commit

---
+++ Variable & experasion/first.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))    
+sum = a + b
+print("The sum of", a, "and", b, "is", sum)
+
+name = input("Enter your name: ")
+print("Hello, " + name + "! Welcome to Python programming.")
